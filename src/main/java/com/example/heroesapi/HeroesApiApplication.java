12afcaadaa227f4f365421fd876878118ed22ced
suele <suelen.ads.guimaraes@gmail.com>
2021-11-18 14:24:02 -0300

    Ajusta os espaços dos arquivos.

--- src/main/java/com/example/heroesapi/HeroesApiApplication.java
+++ src/main/java/com/example/heroesapi/HeroesApiApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class HeroesApiApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(HeroesApiApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(HeroesApiApplication.class, args);
+    }
 
 }
