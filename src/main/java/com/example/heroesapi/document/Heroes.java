810a11e3b00b36bcfd4c2f23e43927cec4a63aeb
suele <suelen.ads.guimaraes@gmail.com>
2021-11-18 15:17:05 -0300

    Ajusta os espaços no arquivo.

--- src/main/java/com/example/heroesapi/document/Heroes.java
+++ src/main/java/com/example/heroesapi/document/Heroes.java
@@ -2,71 +2,63 @@ package com.example.heroesapi.document;
 
 import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBAttribute;
 import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBHashKey;
-import org.springframework.data.annotation.Id;
 import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBTable;
-
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
 
 @Data
-
 @NoArgsConstructor
-@DynamoDBTable(tableName ="Heroes_Api_Table")
-
-
+@DynamoDBTable(tableName = "Heroes_Api_Table")
 public class Heroes {
-  public String getId() {
-    return id;
-  }
+    @Id
+    @DynamoDBHashKey(attributeName = "id")
+    private String id;
+    @DynamoDBAttribute(attributeName = "name")
+    private String name;
+    @DynamoDBAttribute(attributeName = "universe")
+    private String universe;
+    @DynamoDBAttribute(attributeName = "films")
+    private int films;
 
-  public void setId(String id) {
-    this.id = id;
-  }
+    public Heroes(String id, String name, String universe, int films) {
+        this.id = id;
+        this.name = name;
+        this.universe = universe;
+        this.films = films;
+    }
 
-  public String getName() {
-    return name;
-  }
+    public String getId() {
+        return id;
+    }
 
-  public void setName(String name) {
-    this.name = name;
-  }
+    public void setId(String id) {
+        this.id = id;
+    }
 
-  public String getUniverse() {
-    return universe;
-  }
+    public String getName() {
+        return name;
+    }
 
-  public void setUniverse(String universe) {
-    this.universe = universe;
-  }
+    public void setName(String name) {
+        this.name = name;
+    }
 
-  public int getFilms() {
-    return films;
-  }
+    public String getUniverse() {
+        return universe;
+    }
 
-  public void setFilms(int films) {
-    this.films = films;
-  }
+    public void setUniverse(String universe) {
+        this.universe = universe;
+    }
 
-  @Id
-  @DynamoDBHashKey (attributeName = "id")
-  private String id;
+    public int getFilms() {
+        return films;
+    }
 
-  @DynamoDBAttribute (attributeName = "name")
-  private String name;
-
-  @DynamoDBAttribute (attributeName = "universe")
-  private String universe;
-
-  @DynamoDBAttribute (attributeName = "films")
-  private int films;
-
-
-  public Heroes(String id, String name, String universe, int films) {
-    this.id = id;
-    this.name = name;
-    this.universe = universe;
-    this.films = films;
-  }
+    public void setFilms(int films) {
+        this.films = films;
+    }
 
 }
 
